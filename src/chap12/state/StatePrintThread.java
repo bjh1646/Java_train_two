861ae0e6548623d6ad3ccb09efb7e5152ab30b4b
bjh1646 <byonkk@naver.com>
2021-07-26 09:45:03 +0900

    chap12 - 자동 줄바꿈

--- src/chap12/state/StatePrintThread.java
+++ src/chap12/state/StatePrintThread.java
@@ -1,30 +1,31 @@
 package chap12.state;
 
-public class StatePrintThread  extends Thread {
+public class StatePrintThread extends Thread {
 
 	private Thread targetThread;
-	
+
 	public StatePrintThread(Thread targetThread) {
 		this.targetThread = targetThread;
 	}
-	
+
 	public void run() {
-		while(true) {
+		while (true) {
 			Thread.State state = targetThread.getState();
 			System.out.println("타겟 스레드 상태:" + state);
-			
-			if(state == Thread.State.NEW) {
+
+			if (state == Thread.State.NEW) {
 				targetThread.start();
-				
-			if(state == Thread.State.TERMINATED) {
-				break;
+
+				if (state == Thread.State.TERMINATED) {
+					break;
+				}
+
+				try {
+					// 0.5초간 일시 정지
+					Thread.sleep(500);
+				} catch (Exception e) {
+				}
 			}
-			
-			try {
-				//0.5초간 일시 정지
-				Thread.sleep(500);
-			} catch (Exception e) {}
 		}
-	}
 	}
 }
