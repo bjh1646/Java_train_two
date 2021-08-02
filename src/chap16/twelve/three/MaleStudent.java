13fc0db72cc8d425ff9613522a3a251c92680ee6
byonk <byonk@192.168.200.101>
2021-08-02 18:03:31 +0900

    chap16_12_3-병렬 스트림 생성

---
+++ src/chap16/twelve/three/MaleStudent.java
@@ -0,0 +1,27 @@
+package chap16.twelve.three;
+
+import java.util.*;
+
+public class MaleStudent {
+
+	private List<Student> list;
+	
+	public MaleStudent() {
+		list = new ArrayList<Student>();
+		System.out.println("[" + Thread.currentThread().getName() + "] MaleStudent()");
+	}
+	
+	public void accumulate(Student student) {
+		list.add(student);
+		System.out.println("[" + Thread.currentThread().getName() + "] accumulate()");
+	}
+	
+	public void combine(MaleStudent other) {
+		list.addAll(other.getList());
+		System.out.println("[" + Thread.currentThread().getName() + "] MaleStudent()");
+	}
+	
+	public List<Student> getList() {
+		return list;
+	}
+}
